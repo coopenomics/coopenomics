6ff9a9523ec8520201066fb676010342a366a989
Alex Ant <dacom.dark.sun@gmail.com>
2024-07-22 10:41:40 +0500

    update

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+# Базовый путь из .env
+source .env
+BASE_PATH=$BASE_PATH
+
 # Сборка документации
 mkdocs build
 
 # Переключаемся в директорию contracts, генерируем документацию и копируем
-cd ../contracts || exit
+cd "$BASE_PATH/contracts" || exit
 doxygen
-mkdir -p ../doctrine/site/contracts
-rsync -r docs/html/* ../doctrine/site/contracts
+mkdir -p "$BASE_PATH/doctrine/site/contracts"
+rsync -r docs/html/* "$BASE_PATH/doctrine/site/contracts"
 
 # Переключаемся в директорию cooptypes, генерируем документацию и копируем
-cd ../cooptypes || exit
-pnpm docs
-mkdir -p ../doctrine/site/cooptypes
-rsync -r docs/* ../doctrine/site/cooptypes
+cd "$BASE_PATH/monocoop/components/cooptypes" || exit
+pwd
+pnpm run docs
+mkdir -p "$BASE_PATH/doctrine/site/cooptypes"
+rsync -r docs/* "$BASE_PATH/doctrine/site/cooptypes"
 
 # Переключаемся в директорию coopback, генерируем документацию и копируем
-cd ../coopback || exit
-pnpm docs
-mkdir -p ../doctrine/site/coopback
-rsync -r docs/* ../doctrine/site/coopback
+cd "$BASE_PATH/monocoop/components/coopback" || exit
+pnpm run docs
+mkdir -p "$BASE_PATH/doctrine/site/coopback"
+rsync -r docs/* "$BASE_PATH/doctrine/site/coopback"
 
 # Возвращаемся в первоначальную директорию и публикуем документацию
-cd ../doctrine || exit
-pnpm docs-publish
+cd "$BASE_PATH/doctrine" || exit
+#pnpm docs-publish
 
